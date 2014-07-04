aed735d7df579514919e4c400e5ea7c5d04e4c88
NathanGRomano <nathan.g.romano@gmail.com>
2014-07-04 13:39:15 -0400

    implementes fns method

--- gopress.go
+++ gopress.go
@@ -1,9 +1,24 @@
 package gopress
 
+import "container/list"
+
 func Gopress() *Router {
 	var router = new(Router)
 	return router
 }
 
 type Router struct {
+	fns *list.List
+}
+
+func (r *Router) Fns() *list.List {
+	if r.fns == nil {
+		r.fns = list.New()
+	}
+	return r.fns
+}
+
+func (r *Router) Use(fn func()) *Router {
+	r.Fns().PushBack(fn)
+	return r
 }
--- gopress_test.go
+++ gopress_test.go
@@ -8,3 +8,14 @@ func TestGopress(t *testing.T) {
 		t.Error("App should be defined")
 	}
 }
+
+func TestUse(t *testing.T) {
+	var app = Gopress()
+	var fn = func() {}
+	if x := app.Use(fn); x != app {
+		t.Error("Expected app to return itself")
+	}
+	if app.fns.Len() != 1 {
+		t.Error("Expected fns length to be \"1\"")
+	}
+}
